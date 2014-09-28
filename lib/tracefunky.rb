b223e84fa1df75ea6efa21e1a8e674f6bdb208d0
Dan Hensgen <dan@methodhead.com>
2014-09-27 22:37:17 -0400

    * extract Trace

--- lib/tracefunky.rb
+++ lib/tracefunky.rb
@@ -7,8 +7,40 @@ module Tracefunky
     end
   end
 
-  def self.trace
-    root_call = Call.new("ROOT", "ROOT", [])
+  class Trace
+    private
+    attr_reader :root_call, :current_call, :call_stack
+    public
+
+    def initialize
+      @root_call = Call.new("ROOT", "ROOT", [])
+      @current_call = @root
+      @call_stack = []
+    end
+
+    def run
+      set_trace_func Proc.new { |event, file, line, id, the_binding, classname|
+#        if event == "call"
+#          call = Call.new(klass, id, [])
+#          context.current_call[:calls].push(call)
+#          context.stack.push(context.current_call)
+#          context.current_call = call
+#        elsif event == "return"
+#          context.current_call = context.stack.pop
+#        end
+      }
+
+      yield
+
+      set_trace_func nil
+
+      root_call
+    end
+  end
+
+  def self.trace(&block)
+    trace = Trace.new
+    root_call = trace.run(&block)
     FileUtils.mkdir ".tracefunky" unless File.exists?(".tracefunky")
     File.open(".tracefunky/trace.js", "w") do |out|
       JSON.dump(root_call.to_hash, out)
