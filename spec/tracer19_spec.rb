119f518d45c89b91d2aeb86f5b7817b9c3d10cd3
Dan Hensgen <dan@methodhead.com>
2014-10-25 15:36:25 -0400

    Add TestTrace

--- spec/tracer19_spec.rb
+++ spec/tracer19_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 require 'tracefunky/tracer19'
 
+# A trace that just collects information from the tracer to verify in specs.
+#
+class TestTrace
+  attr_reader :events
+
+  def initialize
+    @events = []
+  end
+
+  def call(class_name, method_name)
+    @events << {
+      :type => :call,
+      :class_name => class_name,
+      :method_name => method_name
+    }
+  end
+end
+
 describe "Tracer19" do
   describe "#run" do
     let(:tracer) { Tracefunky::Tracer19.new }
-    let(:trace) { nil }
+    let(:trace) { TestTrace.new }
 
     it "should trace nothing" do
       tracer.run(trace) do
