7ea96abc4fe16e35dc89b4591ab02b6e5048e6cb
Dan Hensgen <dan@methodhead.com>
2014-10-25 08:52:19 -0400

    Trace stuff and print it to structured text; pretty broken

--- spec/tracefunky_spec.rb
+++ spec/tracefunky_spec.rb
@@ -2,6 +2,16 @@ require 'bundler/setup'
 require 'minitest/autorun'
 require 'pry'
 require 'tracefunky'
+require 'json'
+
+class TestClass
+  def simple_method
+    simple_method2
+  end
+
+  def simple_method2
+  end
+end
 
 describe "Tracefunky" do
   before do
@@ -11,6 +21,8 @@ describe "Tracefunky" do
     FileUtils.rm_r(scratch_dir) if File.exists?(scratch_dir)
     FileUtils.mkdir scratch_dir
     FileUtils.cd scratch_dir
+
+    @test_class = TestClass.new
   end
 
   after do
@@ -34,5 +46,47 @@ describe "Tracefunky" do
     end
 
     File.exists?(".tracefunky").must_equal true
+  end
+
+  it "must trace an empty block" do
+    Tracefunky.trace do
+    end
+
+    File.exists?(".tracefunky/trace.out").must_equal true
+    trace = JSON.parse(File.read(".tracefunky/trace.out"))
+    trace.must_equal({"class_name" => "ROOT", "method_name" => "ROOT", "calls" => []})
+  end
+
+  it "must trace a simple method call" do
+    Tracefunky.trace do
+      @test_class.simple_method
+      @test_class.simple_method2
+    end
+
+    trace = JSON.parse(File.read(".tracefunky/trace.out"))
+    trace.must_equal(
+      {
+        "class_name" => "ROOT",
+        "method_name" => "ROOT",
+        "calls" => [
+          {
+            "class_name" => "TestClass",
+            "method_name" => "simple_method",
+            "calls" => [
+              {
+                "class_name" => "TestClass",
+                "method_name" => "simple_method2",
+                "calls" => []
+              }
+            ]
+          },
+          {
+            "class_name" => "TestClass",
+            "method_name" => "simple_method2",
+            "calls" => []
+          }
+        ]
+      }
+    )
   end
 end
