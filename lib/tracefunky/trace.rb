e2308d93f93c75d1cc5f5297a2dc4274d0567e50
Dan Hensgen <dan@methodhead.com>
2014-10-25 18:51:28 -0400

    Stack#call

--- lib/tracefunky/trace.rb
+++ lib/tracefunky/trace.rb
@@ -7,6 +7,15 @@ module Tracefunky
 
     def initialize
       @root = Call.new(nil, nil, [])
+      @current = @root
+      @stack = []
+    end
+
+    def call(class_name, method_name)
+      call = Call.new(class_name, method_name, [])
+      @current.calls.push(call)
+      @stack.push(@current)
+      @current = call
     end
   end
 end
--- spec/trace_spec.rb
+++ spec/trace_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper'
 require 'tracefunky/trace'
 
 describe "Trace" do
-  describe "#initialize" do
-    it "initializes with a root call" do
-      trace = Tracefunky::Trace.new
+  let(:trace) { Tracefunky::Trace.new }
 
+  describe "#initialize" do
+
+    it "initializes with a root call" do
       call = trace.root
       call.wont_be_nil
       call.class_name.must_equal nil
       call.method_name.must_equal nil
+      call.calls.must_equal []
+    end
+  end
+
+  describe "#call" do
+    it "pushes a new call on the trace" do
+      trace.call("TestClass", "meth")
+
+      call = trace.root
+      call.calls.length.must_equal 1
+
+      call = call.calls[0]
+      call.class_name.must_equal "TestClass"
+      call.method_name.must_equal "meth"
       call.calls.must_equal []
     end
   end
